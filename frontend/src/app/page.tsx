f0046cee3b3a24be2e31bea4a5f81ef445ad4953
David L <david1.lin@epitech.eu>
2025-10-17 10:19:35 +0200

    feat(front) : swap token component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,20 @@
 import Header from "./header/header";
+import Swap from "./swap/swap";
 
 export default function Home() {
-  
   return (
-    <Header></Header>
+    <div style={{ backgroundColor: "#190f5c60", minHeight: "100vh" }}>
+      <Header />
+      <main
+        style={{
+          height: "calc(100vh - 64px)",
+          overflowY: "auto",
+          WebkitOverflowScrolling: "touch",
+          padding: "1rem",
+        }}
+      >
+        <Swap />
+      </main>
+    </div>
   );
 }
