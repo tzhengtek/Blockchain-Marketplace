f18f7482b5cae96297d2959e68130cabc8a2dccd
tzheng <timothe.zheng@epitech.eu>
2025-10-24 10:19:44 +0200

    feat(tracing): add more tracing

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -8,14 +8,23 @@ use blockchain::BlockchainIndexer;
 use config::Settings;
 use database::DatabasePool;
 
+use tracing_subscriber::filter::EnvFilter;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{fmt, layer::SubscriberExt};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
     let settings = match Settings::load() {
         Ok(setting) => setting,
         Err(e) => panic!("{}", e),
     };
 
-    println!("{:?}", settings);
+    tracing::info!("{:?}", settings);
+    tracing::info!("POOL");
     let pool = DatabasePool::new(&settings.database_url).await?;
     let indexer = BlockchainIndexer::new(&settings).await?;
     let _ = indexer.fetch(settings.contract, pool).await?;
